ee4d5b55d6493326e8e9dd2fbc347b322f47740d
Ricardo Mendoza Huesca <ingsisrmh@gmail.com>
2015-03-09 17:04:12 -0600

    conexion.php
    Actualización de la función mysql a mysqli para el la conexión a mysql.

---
+++ conexion.php
@@ -0,0 +1,34 @@
+<?php
+
+function consultar($query)
+{
+	$host = 'localhost'; //Escribir el host
+	$user = 'root'; //Escribir el usuario
+	$password = ''; //Escribir el password
+	$database = 'kaisen'; //Escribir el nombre de la base de datos
+	$result = null; //No tocar, es el valor devuelto en caso de no encontrar datos
+
+	$link = mysqli_connect($host, $user, $password) or die('No se pudo conectar: ' . mysqli_error());
+
+	mysqli_select_db($link, $database) or die('No se pudo seleccionar la base de datos');
+	$response = mysqli_query($link, $query);
+	$c = 0;
+
+	while( $row = mysqli_fetch_assoc($response))
+	{
+		$result[$c] = $row;
+		$c++;
+	}
+	return($result);
+	mysqli_close($link);
+}
+
+/* FORMA DE USO
+	require("conexion.php");
+	$consulta = "SELECT * FROM `aseguramiento` WHERE 1";
+	$resultado = consultar($consulta);
+
+  var_dump($resultado);
+*/
+
+?>
